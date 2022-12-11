dc5e1a2a3bbe0652e32dc0b1bee2dd607a3913fa
briant24 <briantadr24@gmail.com>
2022-12-11 18:14:14 +0700

    Dashboard Teknisi

--- app/src/main/java/com/michi/manajemenbengkel/gold/MainActivity.java
+++ app/src/main/java/com/michi/manajemenbengkel/gold/MainActivity.java
@@ -3,12 +3,24 @@ package com.michi.manajemenbengkel.gold;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView nama, tanggal;
+    String snama, currentdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nama = findViewById(R.id.intNama);
+        tanggal = findViewById(R.id.txt_tgl);
+
+        Calendar calendar = Calendar.getInstance();
+        currentdate = DateFormat.getDateInstance(android.icu.text.DateFormat.FULL).format(calendar.getTime());
+        tanggal.setText(currentdate);
     }
 }
